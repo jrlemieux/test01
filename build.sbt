c1df5b4c079400fecc065f6a17ede7eccf1b1d7a
Jacques <jacques@lemieux.cc>
2023-11-02 11:22:29 -0400

    For Jetbrains

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "test"
+
+version := "1.0"
+
+scalaVersion := "3.3.1"
---
+++ src/main/scala/test/Main.scala
@@ -0,0 +1,8 @@
+package test
+
+object Main:
+
+  private def run(args: Array[String]): Unit =
+    println("Hi")
+
+  def main(args: Array[String]): Unit = run(args)
